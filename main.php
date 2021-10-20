18dd15365e08dcd56f0b48cc1ae7379ff1df6e9a
usukhbayar11 <88273271+usukhbayar11@users.noreply.github.com>
2021-10-20 23:59:19 +0800

    Add files via upload

---
+++ main.php
@@ -0,0 +1,30 @@
+<html>
+<style>
+body {
+  background: linear-gradient(90deg, #1CB5E0 0%, #000851 100%);
+}
+</style> 
+<head>
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="icon" href="uwu2.jpg" type="image/gif" sizes="256x256">
+  <link rel="stylesheet"
+  href="https://fonts.googleapis.com/css?family=Open+Sans">
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" href="path/to/font-awesome/css/css.css">
+  <title>Sign in</title>
+</head>
+
+<body>
+  <div class="main">
+    <p class="sign" align="center">Sign in</p>
+    <form class="form1">
+      <input class="un " type="text" align="center" placeholder="Username">
+      <input class="pass" type="password" align="center" placeholder="Password">
+      <a class="submit" align="center" href="completed.php">Sign in</a>
+      <a class="register" align="center"><a href="register.php">New Here</a>
+      <a class="forgot" align="center"><a href="forgor.php">i forgor💀</a>           
+    </div>
+     
+</body>
+
+</html>
---
+++ school stuffffff/0.php
@@ -0,0 +1,22 @@
+<html>
+
+    <head>
+      <title>Sign in</title>
+      <link rel="icon" href="uwu2.jpg" type="image/gif" sizes="256x256">
+    </head>
+    
+    <body>
+      <div class="main">
+        <p class="sign" align="center">Sign in</p>
+        <form class="form1">
+          <input class="un " type="text" align="center" placeholder="Username">
+          <input class="pass" type="password" align="center" placeholder="Password">
+          <a class="submit" align="center">Sign in</a>
+          <p class="forgot" align="center"><a href="#">Forgot Password?</p>
+                
+                    
+        </div>
+         
+    </body>
+    
+    </html>
